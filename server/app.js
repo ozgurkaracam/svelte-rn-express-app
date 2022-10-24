a5fb5ccb6f84194e88c880c678e26db05eec70f5
ozgurkaracam <ozgurkaracam55@gmail.com>
2022-10-24 11:16:33 +0300

    www.js

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ var indexRouter = require('./routes/index');
 var app = express();
 app.use(cors())
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());

a0a1c3ed4b2bf189533ed91866115b0d2e9ba2cb
ozgurkaracam <ozgurkaracam55@gmail.com>
2022-10-25 09:05:23 +0300

    render

--- client-web/src/baseURL.js
+++ client-web/src/baseURL.js
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV=='production' ? "https://vercel.com/ozgurkaracam/svelte-rn-express-app/api/" : "http://localhost:3000/api/"
+export default process.env.NODE_ENV=='production' ? "https://svelte-express.onrender.com/api/" : "http://localhost:3000/api/"
